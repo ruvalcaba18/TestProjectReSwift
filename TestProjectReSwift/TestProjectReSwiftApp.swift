25fbe015cbba3c8f69c3fca15457efab7613aa06
JaelRealtor <jael.ruvalcaba@wizeline.com>
2024-12-18 14:07:34 -0800

    Initial Commit

---
+++ TestProjectReSwift/TestProjectReSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectReSwiftApp.swift
+//  TestProjectReSwift
+//
+//  Created by Wizeline on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectReSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
